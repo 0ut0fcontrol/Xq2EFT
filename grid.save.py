6b9851e0746b009e0563e3702c4d0a7bf408ebbd
yangjincai <yangjincai@nibs.ac.cn>
2017-03-13 10:44:37 +0800

    change name for clearer

--- grid.save.py
+++ grid.save.py
@@ -4,8 +4,5 @@ from eft_calculator import EFT_calculator
 
 calculator = EFT_calculator()
 calculator.setup()
-# Please change the following code to whatever needed to generate the input 
-# coordinates files
-# Please make sure to carry the id number along with the results
 calculator.grid.save("grid.dat")
 
